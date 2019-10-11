076c7e1826cc947c699817cac0f04a837154443c
Ryan Ballenger <Ryan@Ryans-MacBook-Pro.local>
2019-10-11 17:32:34 -0400

    first two

---
+++ NumberGuess/app/src/main/java/com/example/numberguess/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.numberguess
+
+import android.os.Bundle
+import android.text.Editable
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        newGame()
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+    private var answer :Int = -1;
+
+    public fun checkAnswer(view :android.view.View){
+
+        var ans = answer_input.text.toString().toInt()
+
+        if(ans > answer){
+            textView4.text = "Wrong, less"
+        }else if(ans < answer){
+            textView4.text = "Wrong, greater"
+        }else{
+            textView4.text = "Correct! To play again, enter a number"
+            newGame()
+        }
+
+    }
+
+
+    public fun newGame(){
+
+        answer = (1..1000).random()
+
+    }
+
+
+
+}
